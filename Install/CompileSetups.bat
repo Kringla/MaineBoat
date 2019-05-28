2331302a98828b029b4368832476a4232d8ec8ed
Kringla <41374076+Kringla@users.noreply.github.com>
2019-05-28 08:53:12 +0200

    Setup for PVSTest

--- Install/CompileSetups.bat
+++ Install/CompileSetups.bat
@@ -1,11 +1,11 @@
 "C:\Program Files (x86)\NSIS\makensis.exe" M314AdminSetup.nsi
-"C:\Program Files (x86)\NSIS\makensis.exe" M314PMSSetup.nsi
+rem "C:\Program Files (x86)\NSIS\makensis.exe" M314PMSSetup.nsi
 "C:\Program Files (x86)\NSIS\makensis.exe" M314SponsorSetup.nsi
 "C:\Program Files (x86)\NSIS\makensis.exe" M314SelskapSetup.nsi
 "C:\Program Files (x86)\NSIS\makensis.exe" M314PVSSetup.nsi
-
+"C:\Program Files (x86)\NSIS\makensis.exe" M314PVSTestSetup.nsi
 "C:\Program Files (x86)\NSIS\makensis.exe" M314AdminTestSetup.nsi
-"C:\Program Files (x86)\NSIS\makensis.exe" M314PMSTestSetup.nsi
+rem "C:\Program Files (x86)\NSIS\makensis.exe" M314PMSTestSetup.nsi
 "C:\Program Files (x86)\NSIS\makensis.exe" M314SponsorTestSetup.nsi
 "C:\Program Files (x86)\NSIS\makensis.exe" M314SelskapTestSetup.nsi
 REM pause
--- Install/_M314CompileAndUploadALL.bat
+++ Install/_M314CompileAndUploadALL.bat
@@ -1,8 +1,7 @@
 Call M314AdminTestSetup.bat
 Call M314AdminSetup.bat
-Call M314PMSTestSetup.bat
-Call M314PMSSetup.bat
+Call M314PVSTestSetup.bat
+Call M314PVSSetup.bat
 Call M314SelskapTestSetup.bat
 Call M314SelskapSetup.bat
-Call M314PVSSetup.bat
 Call M314SponsorSetup.bat
