490c2117a82ba00221fa0af60664772337d31e8d
Kringla <41374076+Kringla@users.noreply.github.com>
2019-06-26 14:27:03 +0200

    ZRapporter omdannet

---
+++ VBProjects/ExpenseIT/ExpenseITHome.xaml.vb
@@ -0,0 +1,8 @@
+﻿Class ExpenseITHome
+    Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        ' View Expense Report
+        Dim expenseReportPage As New ExpenseReportPage(Me.peopleListBox.SelectedItem)
+        Me.NavigationService.Navigate(expenseReportPage)
+
+    End Sub
+End Class
